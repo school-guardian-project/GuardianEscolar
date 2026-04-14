0ffad108dbf50c8408d7dd89daa8a257baf48cbe
JuanP <jpchalaramirez@gmail.com>
2026-04-14 13:17:01 -0500

    refactor: corrected the use of the fluent API

--- dvlp-back/src/backend/Configuration/PersonConfiguration.cs
+++ dvlp-back/src/backend/Configuration/PersonConfiguration.cs
@@ -8,6 +8,13 @@ namespace backend.Configuration
     {
         public void Configure(EntityTypeBuilder<Person> builder)
         {
+            builder.HasKey(pe => pe.personId);
+            builder.Property(pe => pe.name).HasMaxLength(255);
+            builder.Property(pe => pe.lastName).HasMaxLength(255);
+            builder.Property(pe => pe.identificationType).HasMaxLength(255);
+            builder.Property(pe => pe.identificationNumber).HasMaxLength(255);
+            builder.Property(pe => pe.email).HasMaxLength(255);
+            builder.Property(pe => pe.residenceAddress).HasMaxLength(255);
             builder.HasMany(pe => pe.Profiles).WithOne(pro => pro.Person).HasForeignKey(pro => pro.PersonId);
         }
     }
--- dvlp-back/src/backend/Entities/Person.cs
+++ dvlp-back/src/backend/Entities/Person.cs
@@ -5,27 +5,20 @@ namespace backend.Entities
 {
     public class Person
     {
-        [Key]
-        public int Id { get; set; }
-        // Convencion para aplicar limite de caracteres a un campo
-        [StringLength(maximumLength: 255)]
+        public int personId { get; set; }
+
         public string? name { get; set; }
 
-        [StringLength(maximumLength: 255)]
         public string? lastName { get; set; }
 
-        [StringLength(maximumLength: 255)]
         public string? identificationType { get; set; }
 
-        [StringLength(maximumLength: 255)]
         public string? identificationNumber { get; set; }
 
-        [StringLength(maximumLength: 255)]
         public string? email { get; set; }
 
         public int? phone { get; set; }
 
-        [StringLength(maximumLength: 255)]
         public string? residenceAddress { get; set; }
 
         public ICollection<Profile> Profiles { get; set; } = new List<Profile>();

c766f2d915bbcf2b2c2a6c5e2aca435ac243e268
Sharik <sharikrojas.sena@gmail.com>
2026-04-18 21:10:20 -0500

    Style: finished view information admin and create component sidedar-admin

--- dvlp-front/dvlp-web/src/app/features/profile/pages/information-admin/information-admin.ts
+++ dvlp-front/dvlp-web/src/app/features/profile/pages/information-admin/information-admin.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { SidebarAdmin } from '../../../../shared/components/navbar/sidebar-admin/sidebar-admin';
+
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-information-admin',
   imports: [
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    SidebarAdmin,
   ],
   templateUrl: './information-admin.html',
   styleUrl: './information-admin.css',
@@ -19,4 +22,20 @@ export class InformationAdmin {
   logout() {
     this.router.navigate(['/dashboard-admin']);
   }
+
+  imageUrl: string | ArrayBuffer | null = null;
+
+onFileSelected(event: any) {
+  const file = event.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.imageUrl = reader.result;
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
 }

342fdd2a7d0a58d8d7c2f64082e73497c4fcc9d5
JuanP <jpchalaramirez@gmail.com>
2026-04-21 16:31:18 -0500

    feat: deploy a custom image to dockerize the project backend

--- dvlp-front/dvlp-web/src/app/app.ts
+++ dvlp-front/dvlp-web/src/app/app.ts
@@ -1,29 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './features/public/home/home';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Weatherforecast } from './weatherforecast';
 
 @Component({
   selector: 'app-root',
    imports: 
-   [
-    Home, 
-    RouterOutlet
-  ],
+   [Home, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Guardian Escolar');
-
-  weatherForeCastService = inject(Weatherforecast);
-
-  climas: any[] = [];
-
-  constructor() {
-    this.weatherForeCastService.obtenerClima().subscribe(data => {
-      this.climas = data;
-    });
-  }
 }

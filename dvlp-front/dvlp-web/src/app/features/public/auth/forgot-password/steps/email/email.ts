893db711de1495f3cd6a2c9adeca0f1f073d2ecb
JuanP <jpchalaramirez@gmail.com>
2026-04-22 17:07:23 -0500

    style(forgot-password/email): modify styles for a better interface

--- dvlp-front/dvlp-web/src/app/features/public/auth/forgot-password/steps/email/email.ts
+++ dvlp-front/dvlp-web/src/app/features/public/auth/forgot-password/steps/email/email.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { Footer } from '../../../../../../shared/footer/footer';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavComponent } from '../../../../../../shared/components/navbar/nav-component/nav-component';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-email',
-  imports: [NavComponent, Footer, MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
+  imports: [NavComponent, MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './email.html',
   styleUrl: './email.css',
 })
@@ -22,5 +21,9 @@ export class Email {
 
   onSubmit() {
     this.router.navigate(['../code'], { relativeTo: this.route });
+  }
+
+  login() {
+    this.router.navigate(['/auth/login']);
   }
 }

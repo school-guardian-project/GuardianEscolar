8b4c74467eac8b94ec9d0de510a3fd0bbb7ac432
JuanP <jpchalaramirez@gmail.com>
2026-04-22 17:42:00 -0500

    style(forgot-password/email): implement mail requirement

--- dvlp-front/dvlp-web/src/app/features/public/auth/forgot-password/steps/email/email.ts
+++ dvlp-front/dvlp-web/src/app/features/public/auth/forgot-password/steps/email/email.ts
@@ -3,24 +3,30 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavComponent } from '../../../../../../shared/components/navbar/nav-component/nav-component';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-email',
-  imports: [NavComponent, MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [NavComponent, MatIconModule, MatButtonModule, MatToolbarModule, NgIf, FormsModule],
   templateUrl: './email.html',
   styleUrl: './email.css',
 })
 export class Email {
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-  ) {
-    console.log('Email component initialized');
-  }
+  email: string = '';
+  emailError = '';
+
+  constructor(private router: Router) {}
 
   onSubmit() {
-    this.router.navigate(['../code'], { relativeTo: this.route });
+    this.emailError = '';
+
+    if (!this.email) {
+      this.emailError = '*El correo es requerido';
+    } else {
+      this.router.navigate(['/auth/forgot-password/code']);
+    }
   }
 
   login() {

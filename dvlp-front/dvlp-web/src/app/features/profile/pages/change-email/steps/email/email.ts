57f2303344c2e05bfefc85c0d1a523a8987e7803
JuanP <jpchalaramirez@gmail.com>
2026-04-23 13:18:15 -0500

    style: se modificaron estilos de base e implemnto requisito de email

--- dvlp-front/dvlp-web/src/app/features/profile/pages/change-email/steps/email/email.ts
+++ dvlp-front/dvlp-web/src/app/features/profile/pages/change-email/steps/email/email.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
 import { NavComponent } from "../../../../../../shared/components/navbar/nav-component/nav-component";
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { MatIcon, MatIconModule } from "@angular/material/icon";
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-email',
-  imports: [NavComponent],
+  imports: [NavComponent, MatIcon, MatIconModule, FormsModule, NgIf],
   templateUrl: './email.html',
   styleUrl: './email.css',
 })
 export class Email {
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute
-  ) {
-    
-  }
+  email: string = '';
+  emailError: string = '';
+  constructor(private router: Router) {}
 
   onSubmit() {
-    this.router.navigate(['../code-first'], { relativeTo: this.route });
+    this.emailError = '';
+    
+    if (!this.email) {
+      this.emailError = '*El correo es requerido'
+    } else {
+      this.router.navigate(['../code-first']);
+    }
+  }
+
+  adminInfo() {
+    this.router.navigate(['/admin/informacion'])
   }
 }

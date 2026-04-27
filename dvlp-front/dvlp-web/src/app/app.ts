0cf41e74605ad0ae33e7141b72af64886d496995
Sharik <sharikrojas.sena@gmail.com>
2026-04-27 09:33:45 -0500

    feature: The theme change modal is now functional; only style adjustments are needed.

--- dvlp-front/dvlp-web/src/app/app.ts
+++ dvlp-front/dvlp-web/src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './features/public/home/home';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 // import { MatButtonModule } from '@angular/material/button';
 // import { MatCardModule } from '@angular/material/card';
 // import { MatFormFieldModule } from '@angular/material/form-field';
@@ -26,4 +28,27 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class App {
   protected readonly title = signal('Guardian Escolar');
+
+  private platformId = inject(PLATFORM_ID);
+
+setTheme(theme: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.classList.remove(
+        'light-theme-blue',
+        'light-theme-green',
+        'dark-theme-blue',
+        'dark-theme-green'
+      );
+
+      document.body.classList.add(theme);
+    }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedTheme = localStorage.getItem('theme') || 'light-theme-blue';
+      this.setTheme(savedTheme);
+    }
+  }
 }
+

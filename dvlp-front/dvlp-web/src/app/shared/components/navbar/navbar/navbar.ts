8150932d675ffa511093e2513cf51e2a67c773a0
JuanP <jpchalaramirez@gmail.com>
2026-04-29 13:05:34 -0500

    feat: implement method to navigate to contact tab.

--- dvlp-front/dvlp-web/src/app/shared/components/navbar/navbar/navbar.ts
+++ dvlp-front/dvlp-web/src/app/shared/components/navbar/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, computed, HostListener, input, output, signal, ViewChild} from '@angular/core';
+import { Component, computed, HostListener, ViewChild} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -38,5 +38,9 @@ export class Navbar {
 
   login() {
     this.router.navigate(['/auth/login'])
+  }
+
+  contact() {
+    this.router.navigate(['contact']);
   }
 }

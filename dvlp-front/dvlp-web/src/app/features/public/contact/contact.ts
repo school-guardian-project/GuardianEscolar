dafc55c31ea82099b442157a8e6a1f42b7b1316c
JuanP <jpchalaramirez@gmail.com>
2026-04-29 15:31:28 -0500

    save

--- dvlp-front/dvlp-web/src/app/features/public/contact/contact.ts
+++ dvlp-front/dvlp-web/src/app/features/public/contact/contact.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "../../../shared/components/navbar/nav-component/nav-component";
+import { Footer } from "../../../shared/footer/footer";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [NavComponent, Footer],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })

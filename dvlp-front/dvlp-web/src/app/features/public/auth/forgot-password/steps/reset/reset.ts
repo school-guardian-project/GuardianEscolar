a6bb3499b9899a97cbe91ba5afa03dee88022c1b
JuanP <jpchalaramirez@gmail.com>
2026-05-05 09:42:43 -0500

    refactor: implement password strength and format validations

--- dvlp-front/dvlp-web/src/app/features/public/auth/forgot-password/steps/reset/reset.ts
+++ dvlp-front/dvlp-web/src/app/features/public/auth/forgot-password/steps/reset/reset.ts
@@ -1,27 +1,54 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ChangeInformation } from "@shared/components/change/change-information/change-information";
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangePassword } from '@shared/components/change/change-password/change-password';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { passwordMatch } from '@shared/validator/password-match.validator';
 
 @Component({
   selector: 'app-reset',
-  imports: [ChangeInformation, FormsModule, ReactiveFormsModule],
+  imports: [ChangePassword, FormsModule, ReactiveFormsModule],
   templateUrl: './reset.html',
   styleUrl: './reset.css',
 })
 export class Reset {
   form: FormGroup;
 
-  constructor(private router: Router, private fb: FormBuilder) {
-    this.form = this.fb.group({
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16), Validators.pattern('^[a-zA-Z0-9]$/g')]]
-    }, {
-      validators: [
-        passwordMatch('password', 'confirmPassword')
-      ]
-    });
+  constructor(
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group(
+      {
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(
+              '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'
+            ),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validators: [passwordMatch('password', 'confirmPassword')],
+      }
+    );
+  }
+  get f() {
+    return this.form.controls;
+  }
+
+  checkRule(regex: string): boolean {
+    const value = this.form.get('password')?.value || '';
+    return new RegExp(regex).test(value);
   }
 
   onSubmit() {

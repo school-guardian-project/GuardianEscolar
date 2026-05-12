22405342a0e36a2f4e6c81c832cd6e5ab6b6bb14
Smith <jsmith.2407san@gmail.com>
2026-05-12 14:57:51 -0500

    fix: correction of observations made

--- dvlp-front/dvlp-web/src/app/features/public/contact/contact.ts
+++ dvlp-front/dvlp-web/src/app/features/public/contact/contact.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from "../../../shared/components/navbar/nav-component/nav-component";
 import { Footer } from "../../../shared/footer/footer";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-contact',
-  imports: [NavComponent, Footer],
+  imports: [NavComponent, Footer, MatIcon],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
--- dvlp-front/dvlp-web/src/app/shared/components/navbar/nav-component/nav-component.ts
+++ dvlp-front/dvlp-web/src/app/shared/components/navbar/nav-component/nav-component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-nav-component',
@@ -9,4 +10,10 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   templateUrl: './nav-component.html',
   styleUrl: './nav-component.css',
 })
-export class NavComponent {}
+export class NavComponent {
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
+}

c18659de98e7db0a78364a3c3b048e3792430d6d
JuanP <jpchalaramirez@gmail.com>
2026-05-13 09:13:59 -0500

    refactor: A folder structure was organized to implement the proposed architecture, and Swagger was implemented.

--- dvlp-back/src/backend/API/Program.cs
+++ dvlp-back/src/backend/API/Program.cs
@@ -1,18 +1,21 @@
 using backend.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder.Services.AddControllers();
 
 // Arreglo de strings y origenes permitidos
 var allowedOrigins = builder.Configuration.GetValue<string>("AllowedOrigins")!.Split(",");
 
+// Database
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseNpgsql("name=Conection"));
 
+// Cors
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -24,17 +27,37 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddDbContext<AppDbContext>(options => 
-    options.UseNpgsql("name=Conection"));
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Backend API",
+        Version = "v1",
+        Description = "API construida con ASP.NET Core"
+    });
+});
+
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/swagger/v1/swagger.json",
+            "Backend API v1");
+
+        options.RoutePrefix = string.Empty;
+    });
 }
 
+// Swagger
 app.UseHttpsRedirection();
 
 // Activando Cors en toda la aplicacion

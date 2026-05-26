d964c4ce82a1a102429a66156bd551e86f7d61a3
Sharik <sharikrojas.sena@gmail.com>
2026-05-26 16:42:21 -0500

    style:creation of the password recovery views and functional navigation between them

--- dvlp-front/dvlp-movil/app-movil/app-movil/Pages/Auth/NewPassword.xaml.cs
+++ dvlp-front/dvlp-movil/app-movil/app-movil/Pages/Auth/NewPassword.xaml.cs
@@ -1,33 +1,14 @@
 namespace app_movil.Pages.Auth;
 
-public partial class VerifyCode : ContentPage
+public partial class NewPassword : ContentPage
 {
-	public VerifyCode()
+	public NewPassword()
 	{
 		InitializeComponent();
 	}
     private async void OnSendCodeClicked(object? sender, EventArgs e)
     {
-        await Navigation.PushAsync(new NewPassword());
+        await Navigation.PushAsync(new Welcome());
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

0c66a6f784fc5c1f2baae09a41d8cf96fb721168
JuanP <jpchalaramirez@gmail.com>
2026-05-27 12:44:22 -0500

    advances in user authentication in the application.

---
+++ dvlp-back/src/backend/Application/Interfaces/Common/IJwtService.cs
@@ -0,0 +1,12 @@
+﻿using backend.Domain.Entities.Users;
+
+namespace backend.Application.Interfaces.Common
+{
+    public interface IJwtService
+    {
+        String GenerateToken(
+            Person person,
+            List<String> roles
+        );
+    }
+}

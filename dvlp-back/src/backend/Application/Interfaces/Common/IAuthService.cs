9427b1a726f95bd783c42b78842f3a5291e3ba8d
JuanP <jpchalaramirez@gmail.com>
2026-05-27 17:35:14 -0500

    fix: correct AuthService to match the ORM entities

--- dvlp-back/src/backend/Application/Interfaces/Common/IAuthService.cs
+++ dvlp-back/src/backend/Application/Interfaces/Common/IAuthService.cs
@@ -1,11 +1,12 @@
-﻿using backend.Application.DTOs.Auth;
+﻿using backend.Application.DTOs;
+using backend.Application.DTOs.Auth;
 
 namespace backend.Application.Interfaces.Common
 {
     public interface IAuthService
     {
         Task<LoginResponseDto> LoginAsync(
-            LoginResponseDto request
+            LoginRequestDto request
         );
     }
 }

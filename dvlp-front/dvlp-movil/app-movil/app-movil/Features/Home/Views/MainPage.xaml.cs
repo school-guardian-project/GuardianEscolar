2338cb6ed5e8d32e6b362e696805dd47d6324f15
Sharik <sharikrojas.sena@gmail.com>
2026-06-24 15:55:07 -0500

    feat: add librery OpenStreetMap

--- dvlp-front/dvlp-movil/app-movil/app-movil/Features/Home/Views/MainPage.xaml.cs
+++ dvlp-front/dvlp-movil/app-movil/app-movil/Features/Home/Views/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Mapsui.Tiling;
+using Mapsui;
 namespace app_movil.Features.Home.Views;
 
 public partial class MainPage : ContentPage
@@ -5,5 +7,13 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		LoadMap();
+	}
+
+	private void LoadMap() 
+	{
+		var map = new Mapsui.Map();
+		map.Layers.Add(OpenStreetMap.CreateTileLayer());
+		RouteMap.Map = map;
 	}
 }

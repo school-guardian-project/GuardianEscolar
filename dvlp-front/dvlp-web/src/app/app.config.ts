7f282f53583705d41895f3fe48743999f4a46615
JuanP <jpchalaramirez@gmail.com>
2026-06-24 12:33:46 -0500

    feat: communication and consumption of the API for the validation and authentication of admin users.

--- dvlp-front/dvlp-web/src/app/app.config.ts
+++ dvlp-front/dvlp-web/src/app/app.config.ts
@@ -3,17 +3,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { provideTranslateService, TranslateModule } from '@ngx-translate/core';
 
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { authInterceptor } from '@core/services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideClientHydration(withEventReplay()),
 
 
---
+++ dvlp-front/dvlp-web/src/app/core/services/auth.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+    private apiUrl = `${environment.apiUrl}/api`;
+    
+    constructor(private http: HttpClient) {}
+
+    login(email: string, password: string): Observable<any> {
+        return this.http.post(`${this.apiUrl}/login`, { email, password }).pipe(
+            tap((response: any) => {
+                localStorage.setItem('access_token', response.accessToken);
+                localStorage.setItem('user_name', response.name);
+                localStorage.setItem('user_email', response.email);
+                localStorage.setItem('user_roles', JSON.stringify(response.roles));
+            })
+        );
+    }
+
+    getToken(): string | null {
+        return localStorage.getItem('access_token');
+    }
+
+    isAuthenticated(): boolean {
+        const token = this.getToken();
+        if (!token) {
+            return false;
+        }
+        return true;
+    }
+
+    logout(): void {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('user_email');
+        localStorage.removeItem('user_name');
+        localStorage.removeItem('user_roles');
+    }
+}

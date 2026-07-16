c4983c8595a6450dd9a46a3bce800869f13c7903
Sharik <sharikrojas.sena@gmail.com>
2026-07-16 14:01:06 -0500

    refactor: remove .NET MAUI project after React Native migration

--- dvlp-front/dvlp-movil/app-movil/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Limpiando bin y obj..."
-
-# Elimina las carpetas bin y obj de forma recursiva (-r) y forzada (-f)
-rm -rf bin
-rm -rf obj
-
-echo "Listo!"
--- dvlp-front/dvlp-movil/app-movil/limpiar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Limpiando bin y obj...
-rd /s /q bin
-rd /s /q obj
-echo Listo!
-pause

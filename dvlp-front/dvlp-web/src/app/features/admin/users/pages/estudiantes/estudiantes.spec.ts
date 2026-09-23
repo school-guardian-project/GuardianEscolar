f1e05a9b2de5169ae6985545aac63b38bd8a20ac
Juan Chala <jpchalaramirez@gmail.com>
2026-09-23 09:16:35 -0500

    feat: connect students page to CRUD API

--- dvlp-front/dvlp-web/src/app/features/admin/users/pages/estudiantes/estudiantes.spec.ts
+++ dvlp-front/dvlp-web/src/app/features/admin/users/pages/estudiantes/estudiantes.spec.ts
@@ -1,3 +1,5 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Estudiantes } from './estudiantes';
@@ -9,6 +11,7 @@ describe('Estudiantes', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Estudiantes],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Estudiantes);
